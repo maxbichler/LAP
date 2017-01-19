7de79d5765e642dbc7e0af1d3abd5d382ad6a661
maxbichler <onkelz-2000@hotmail.de>
2017-01-19 09:54:17 +0100

    Delete 07_create_check.sql

--- 07_create_check.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE Unterkunft
-ADD
-CONSTRAINT ck_kategorie
-CHECK ( kategorie >= 0 AND kategorie <= 5 );
-
-ALTER TABLE Bewertung
-ADD
-CONSTRAINT ck_wertung
-CHECK ( wertung >=0 AND wertung <= 5 );
