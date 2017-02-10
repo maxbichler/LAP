c3685a0f959d53eb8025bf70d794b07738ac816c
bichmaxi <bichmaxi@PC901B0E043E51>
2017-02-10 09:45:36 +0100

    layout

---
+++ LAP/innovation4austria.web/innovation4austria.web/Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace innovation4austria.web.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Pflichtfeld", AllowEmptyStrings = false)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Pflichtfeld", AllowEmptyStrings = false)]
+        [StringLength(24, ErrorMessage = "8-16 Zeichen", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        public string Passwort { get; set; }
+    }
+}
